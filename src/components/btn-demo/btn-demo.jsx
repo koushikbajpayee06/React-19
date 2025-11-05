9eea63d4297a0327a1cf5a423dcf6fa873efe539
Koushik <Koushikbajpayee06@gmail.com>
2025-11-05 17:06:01 +0530

    disabling right click

--- src/components/btn-demo/btn-demo.jsx
+++ src/components/btn-demo/btn-demo.jsx
@@ -4,9 +4,15 @@ export function ButtonDemo(){
     function handleDoubleClick(){
         window.open('p1.png',"Mobile",'width=400 height=400')
     }
-    
+
+    function handleContextMenu(){
+        document.oncontextmenu = function(){
+            alert("Right Click not allowed")
+            return false;
+        }
+    }
     return(
-        <div className="container-fluid">
+        <div onContextMenu={handleContextMenu} className="container-fluid">
             <h2>Button Events</h2>
             <img src='p1.png' onClick={handleDoubleClick} width="150" height="150"></img>
             <p>Double Click to view Large</p>
