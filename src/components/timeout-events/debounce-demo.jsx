22ca437c66481426c04443a8c539af395ff642a3
Koushik <Koushikbajpayee06@gmail.com>
2025-11-05 18:01:17 +0530

    handling debounce

--- src/components/timeout-events/debounce-demo.jsx
+++ src/components/timeout-events/debounce-demo.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export function Debounce(){
     
     const [msg, setMsg] = useState(null);
+    let thread = useRef(null);
 
     function v1(){
         setMsg("volume up by 30%");
@@ -12,17 +13,23 @@ export function Debounce(){
     }
     function v3(){
         setMsg("volume full");
-    }
+    }  
+
     function handleBtnClk(){
         setTimeout(v1,4000);
-        setTimeout(v2,8000);
+        thread.current = setTimeout(v2,8000);
         setTimeout(v3,10000)
+    }
+
+    function handleCancelClick(){
+        clearTimeout(thread.current)
     }
 
     return(
         <div className="container-fluid">
             <div className="mt-3">
                 <button onClick={handleBtnClk} className="bi bi-volume-up btn btn-success"></button>
+                <button onClick={handleCancelClick} className="btn btn-danger mx-2">Cancle V2</button>
             </div>
             <h2>{msg}</h2>
         </div>
