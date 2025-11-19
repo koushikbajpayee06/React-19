18df6834f2255e12a9c55f28e6ec68f2f2a39519
Koushik <Koushikbajpayee06@gmail.com>
2025-11-19 14:52:57 +0530

    adding data from child to parent

--- src/Fakestore/FakeStoreIndex.jsx
+++ src/Fakestore/FakeStoreIndex.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { FakeStoreProducts } from "./FakeStoreProducts";
 
 export function FakeStoreIndex(){
+    const [cartItems, setCartItems] = useState([]);  
+    const [cartItemsCount, setCartItemsCount] = useState(0);
+
     function handleaddToCartClick(e){
-        console.log(e);
+        cartItems.push(e);
+        alert(`${e.title}\nAdded to cart`)
+        // console.log(cartItems);
+        setCartItemsCount(cartItems.length)
     }
     return(
         <div className="container-fluid">
@@ -15,7 +22,35 @@ export function FakeStoreIndex(){
                     </div>
                 </nav>
                 <div>
-                    <button className="btn btn-warning bi bi-cart4 position-relative"><span className="position-absolute bg-danger text-white rounded rounded-circle"></span></button>
+                    <button data-bs-target='#cart' data-bs-toggle='offcanvas' className="btn btn-warning bi bi-cart4 position-relative">Your Cart<span className="position-absolute fs-6 badge bg-danger text-white rounded rounded-circle" style={{right:"-10px", top:"-10px"}}>{cartItemsCount}</span></button>
+                    <div className="offcanvas offcanvas-end" id="cart">
+                        <div className="offcanvas-header">
+                            <h3>Your Cart Items</h3>
+                            <button className="btn btn-close" data-bs-dismiss="offcanvas"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <table className="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>PreView</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        cartItems.map((item)=>(
+                                            <tr key={item.id}>
+                                                <td>{item.title}</td>
+                                                <td><img src={item.image} width="50"/></td>
+                                                <td>{item.price}</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </header>
             <section>
